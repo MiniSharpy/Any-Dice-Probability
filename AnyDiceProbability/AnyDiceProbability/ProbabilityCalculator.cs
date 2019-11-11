42c62b96db4482021ec7a308440a9bdb3d6d675c
MiniSharpy <MiniSharpy@users.noreply.github.com>
2019-11-11 18:42:14 +0000

    Basic Functionalitiy

--- AnyDiceProbability/AnyDiceProbability/ProbabilityCalculator.cs
+++ AnyDiceProbability/AnyDiceProbability/ProbabilityCalculator.cs
@@ -6,43 +6,86 @@ using System.Threading.Tasks;
 
 namespace AnyDiceProbability
 {
+    //Need to sort out calculating chances with multiple types of dice. I.E 1d6+2d4
+    //Need to allow for things such as drop lowest or highest.
     public class ProbabilityCalculator
     {
-        Dictionary<int, int> CalculateFrequency(List<int> discreteOutcome, List<List<int>> diceOutcome)
+        public static Dictionary<int, double> CalculateFrequency(int diceQuantity, int dieSides)
         {
-            Dictionary<int, int> frequencies = new Dictionary<int, int>();
+            Dictionary<int, double> frequencies = new Dictionary<int, double>();
+            List<int> discreteOutcomes = GenerateDiscreteOutcomes(diceQuantity, dieSides);
+            List<int> allOutcomes = GenerateAllOutcomes(diceQuantity, dieSides);
 
-            foreach (var dieDiscreteOutcome in diceOutcome)
+            
+            foreach (var dieSide in discreteOutcomes)
             {
-                foreach (var dieSide in dieDiscreteOutcome)
+                int count = 0;
+                foreach (var currentOutcome in allOutcomes)
                 {
-
+                    if (currentOutcome == dieSide)
+                    {
+                        count++;
+                    }
                 }
+                double possibility = (double) count / allOutcomes.Count() * 100;
+                frequencies.Add(dieSide, possibility);
             }
 
             return frequencies;
         }
 
-        public List<int> GenerateDiscreteOutcome(int diceQuantity, int dieSides)
+        public static List<int> GenerateAllOutcomes(int diceQuantity, int dieSides)
         {
-            List<int> discreteOutcome = new List<int>();
+            List<int> dieDiscreteOutcome = GenerateDieDiscreteOutcome(dieSides);
+
+            if (diceQuantity == 1) { return dieDiscreteOutcome; }
+
+            List<int> product = new List<int>();
+            for (int i = 0; i < diceQuantity-1; i++)
+            {
+                product = CartesianProduct(dieDiscreteOutcome, product);
+            }
+
+            return product;
+        }
+
+        private static List<int> CartesianProduct(List<int> dieDiscreteOutcome, List<int> inputProduct)
+        {
+            if (inputProduct.Count == 0)
+            {
+                inputProduct = dieDiscreteOutcome;
+            }
+
+            IEnumerable<int> product = //Cartesian Product
+                from first in inputProduct
+                from second in dieDiscreteOutcome
+                select first + second;
+
+            List<int> finalList = product.ToList();
+
+            return finalList;
+        }
+
+        public static List<int> GenerateDiscreteOutcomes(int diceQuantity, int dieSides)
+        {
+            List<int> discreteOutcomes = new List<int>();
             int minimumOutcome = diceQuantity;
             int maximumOutcome = diceQuantity * dieSides;
 
             for (int i = minimumOutcome; i < maximumOutcome+1; i++)
             {
-                discreteOutcome.Add(i);
+                discreteOutcomes.Add(i);
             }
 
-            return discreteOutcome;
+            return discreteOutcomes;
         }
 
-        public double AmountOfOutcomes(int diceQuantity, int dieSides)//Uses a list of a list so that when calculating frequency for loop can be used maybe.
+        public static double AmountOfOutcomes(int diceQuantity, int dieSides)//Uses a list of a list so that when calculating frequency for loop can be used maybe.
         {
             return Math.Pow(dieSides, diceQuantity);
         }
 
-        public List<int> GenerateDieSides(int dieSides)
+        public static List<int> GenerateDieDiscreteOutcome(int dieSides)//Generates a range of numbers from 1 through to dieSides.
         {
             List<int> dieDiscreteOutcome = new List<int>();//Could to with a clearer name that shows it's all possible outcomes.
             for (int currentSide = 1; currentSide < dieSides + 1; currentSide++)
--- AnyDiceProbability/AnyDiceProbability/Program.cs
+++ AnyDiceProbability/AnyDiceProbability/Program.cs
@@ -10,77 +10,25 @@ namespace AnyDiceProbability
     {
         static void Main(string[] args)
         {
-            ProbabilityCalculator ProbabilityCalculator = new ProbabilityCalculator();
-
-
-
-            
-            var s1 = new[] { 1, 2 };
-
-            IEnumerable<int> product = GenerateDiceOutcome(s1, 2);
-
-            foreach (var item in product)
-            {
-                Console.Write(item);
-            }
+            var final = ProbabilityCalculator.CalculateFrequency(3, 6);
+           
 
             Console.WriteLine();
 
-            int count = 0;
-            foreach (var item in product)
+            foreach (var item in final)
             {
-                if (item == 3)
-                {
-                    count++;
-                }
+                Console.WriteLine(item);
             }
-            double number = (double)count / ProbabilityCalculator.AmountOfOutcomes(2,6);
-            Console.WriteLine(number*100 + "%");
-
-
             Console.Read();
         }
 
-        private static IEnumerable<int> GenerateDiceOutcome(int[] dieSides, int diceQuantity, int[] lastProduct = null)
-        {
-            if(lastProduct == null)
-            {
-                lastProduct = new int[dieSides.Length];
-                dieSides.CopyTo(lastProduct, 0);
-            }
-
-            diceQuantity --;
-
-            IEnumerable<int> product; //Cartesian Product
-            product = 
-                from first in dieSides
-                from second in lastProduct
-                select first + second;
-
-            if (diceQuantity > 0)
-            {
-                List<int> list = new List<int>();
-                foreach (var item in product)
-                {
-                    list.Add(item);
-                }
-                lastProduct = new int[list.Count];
-                list.ToArray().CopyTo(lastProduct, 0);
-
-                GenerateDiceOutcome(dieSides, diceQuantity, lastProduct);
-            }
-
-            return product;
-
-        }
-
-        private static void WriteList<T>(List<T> list)
-        {
-            foreach (var item in list)
-            {
-                Console.Write(item + " ");
-            }
-            Console.Write("\n");
-        }
+        //private static void WriteList<T>(List<T> list)
+        //{
+        //    foreach (var item in list)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+        //    Console.Write("\n");
+        //}
     }
 }
